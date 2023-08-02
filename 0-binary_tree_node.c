9151fb6b1746e8e566419e3f9d128e8f93214db6
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-08-02 15:38:11 +0100

    Binary Trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,9 +9,7 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newTreeNode;
-
-	newTreeNode = malloc(sizeof(binary_tree_t));
+	binary_tree_t *newTreeNode = malloc(sizeof(binary_tree_t));
 
 	if (newTreeNode == NULL)
 		return (NULL);
