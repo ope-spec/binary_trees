c7fa8bc247bf1371a644e3ce448fca0cbf15d6b2
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-08-02 21:07:53 +0100

    Updated Code

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,13 +8,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_left, height_right;
-
 	if (tree == NULL)
 		return (0);
 
-	height_left = binary_tree_height(tree->left);
-	height_right = binary_tree_height(tree->right);
+	size_t height_left = 0, height_right = 0;
 
-	return (height_left > height_right ? height_left + 1 : height_right + 1);
+		height_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		height_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	
+	return ((height_left > height_right) ? height_left : height_right);
+
 }
